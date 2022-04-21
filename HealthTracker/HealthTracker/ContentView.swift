56cc1c1fa369f0259e83c1830f1fc020cce8f7a0
nhinguyen26 <67761714+nhinguyen26@users.noreply.github.com>
2022-04-21 16:46:53 -0700

    Initial Project Files

---
+++ HealthTracker/HealthTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthTracker
+//
+//  Created by Nhi Nguyen on 4/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HealthTracker/HealthTracker/HealthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerApp.swift
+//  HealthTracker
+//
+//  Created by Nhi Nguyen on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
